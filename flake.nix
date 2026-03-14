78e84a292b16eb5a2ff7d997f98f797116ba5cbc
Wölfchen <115360611+W-lfchen@users.noreply.github.com>
2026-03-14 12:18:17 +0100

    feat(flake): use fenix to provide rust toolchain

--- flake.nix
+++ flake.nix
@@ -1,28 +1,35 @@
 {
-  description = "A very basic flake";
+  description = "alfred-2 dev flake";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   outputs =
-    { self, nixpkgs }:
+    {
+      self,
+      fenix,
+      nixpkgs,
+    }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
       devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          cargo
-          clippy
-          rust-analyzer
-          rustc
-          rustfmt
-          sccache
+        buildInputs = [
+          (fenix.packages.${system}.stable.withComponents [
+            "cargo"
+            "clippy"
+            "rust-analyzer"
+            "rust-src"
+            "rustc"
+          ])
         ];
-        RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-        shellHook = ''
-          export RUSTC_WRAPPER=sccache
-        '';
       };
       # currently broken due to how the token is handeled
       #packages.${system} = {
