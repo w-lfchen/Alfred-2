73244d2c84575221e01757c67984f9bc4bf957bd
Wölfchen <w-lfchen@posteo.net>
2026-03-17 15:23:02 +0100

    fix: cleanup; add anyhow
    anyhow is useful because all we really need to do with errors is
    displaying them

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,21 @@ mod errors;
 use crate::errors::NoDolphinError;
 
 use std::{
-    env, fs::File, io::{BufRead, BufReader}
+    fs::File,
+    io::{BufRead, BufReader},
 };
 
 use poise::{
-    serenity_prelude::{
-        self as serenity, Color, CreateAttachment, CreateEmbed, CreateEmbedAuthor,
-        CreateEmbedFooter,
-    },
     CreateReply, Prefix, PrefixFrameworkOptions,
+    serenity_prelude::{
+        ClientBuilder, Color, CreateAttachment, CreateEmbed, CreateEmbedAuthor, CreateEmbedFooter,
+        GatewayIntents,
+    },
 };
 use rand::seq::IteratorRandom;
 use reqwest::Response;
 
-// definitions copied from example
-struct Data {} // User data, which is stored and accessible in all command invocations
-type Error = Box<dyn std::error::Error + Send + Sync>;
-type Context<'a> = poise::Context<'a, Data, Error>;
+type Context<'a> = poise::Context<'a, (), anyhow::Error>;
 
 // TODO: maybe improve this as this path is relative to the current working directory
 const DOLPHIN_PATH: &str = "./resources/dolphins.txt";
@@ -29,7 +27,7 @@ const DOLPHIN_PATH: &str = "./resources/dolphins.txt";
 /// This command fetches you a random cat from https://cataas.com/ :3
 /// More parameters might be added in the future.
 #[poise::command(slash_command, prefix_command, track_edits)]
-async fn cat(ctx: Context<'_>) -> Result<(), Error> {
+async fn cat(ctx: Context<'_>) -> Result<(), anyhow::Error> {
     // fetch a cat :3
     let response = reqwest::get("https://cataas.com/cat").await?;
     // determine filename, default to "cat.jpg"
@@ -66,7 +64,7 @@ fn get_file_extension(response: &Response) -> Option<&str> {
 async fn define(
     ctx: Context<'_>,
     #[description = "Query"] query: Vec<String>,
-) -> Result<(), Error> {
+) -> Result<(), anyhow::Error> {
     let query = match ctx {
         // currently too lazy too properly parse this, this is the easy way out
         Context::Application(_) => query
@@ -119,20 +117,17 @@ async fn define(
 
 /// alfred delfin
 #[poise::command(slash_command, prefix_command, track_edits)]
-async fn delfin(ctx: Context<'_>) -> Result<(), Error> {
+async fn delfin(ctx: Context<'_>) -> Result<(), anyhow::Error> {
     let f = File::open(DOLPHIN_PATH)?;
     let f = BufReader::new(f);
-    let dolphin = f
-        .lines()
-        .choose(&mut rand::rng())
-        .ok_or(NoDolphinError)??;
+    let dolphin = f.lines().choose(&mut rand::rng()).ok_or(NoDolphinError)??;
     ctx.say(dolphin).await?;
     Ok(())
 }
 
 /// alfred eminem
 #[poise::command(slash_command, prefix_command, track_edits)]
-async fn eminem(ctx: Context<'_>) -> Result<(), Error> {
+async fn eminem(ctx: Context<'_>) -> Result<(), anyhow::Error> {
     ctx.say("https://media.tenor.com/-5B-JLo2q1cAAAAC/eminem-now-this-looks-like-a-job-for-me.gif")
         .await?;
     Ok(())
@@ -140,19 +135,18 @@ async fn eminem(ctx: Context<'_>) -> Result<(), Error> {
 
 /// alfred kleanthis
 #[poise::command(slash_command, prefix_command, track_edits)]
-async fn kleanthis(ctx: Context<'_>) -> Result<(), Error> {
+async fn kleanthis(ctx: Context<'_>) -> Result<(), anyhow::Error> {
     ctx.say("https://discordemoji.com/assets/emoji/KannaSip.png")
         .await?;
     Ok(())
 }
 
 #[tokio::main]
-async fn main() {
-    dotenvy::dotenv().unwrap();
+async fn main() -> Result<(), anyhow::Error> {
+    dotenvy::dotenv()?;
     // get some constant parameters
-    let token = env::var("BOT_TOKEN").unwrap();
-    let intents =
-        serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT;
+    let token = std::env::var("BOT_TOKEN")?;
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
 
     // create framework
     let framework = poise::Framework::builder()
@@ -162,10 +156,7 @@ async fn main() {
             // set up prefix
             prefix_options: PrefixFrameworkOptions {
                 prefix: Some(String::from("alfred")),
-                additional_prefixes: vec![Prefix::Regex(
-                    regex::Regex::new(r"(?i)\balfred\b")
-                        .expect("error(prefixes): regex compilation failed for some reason"),
-                )],
+                additional_prefixes: vec![Prefix::Regex(regex::Regex::new(r"(?i)\balfred\b")?)],
                 mention_as_prefix: true,
                 execute_untracked_edits: true,
                 ignore_bots: false,
@@ -174,18 +165,17 @@ async fn main() {
             },
             ..Default::default()
         })
-        // TODO: figure out what this does (copied from example)
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-                Ok(Data {})
+                Ok(())
             })
         })
         .build();
-    // get the client
-    let client = serenity::ClientBuilder::new(token, intents)
+    ClientBuilder::new(token, intents)
         .framework(framework)
-        .await;
-    // start the thing
-    client.unwrap().start().await.unwrap();
+        .await?
+        .start()
+        .await?;
+    Ok(())
 }
